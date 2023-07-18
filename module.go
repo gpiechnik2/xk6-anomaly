4e80c5aabcb438b4173bc8b87b3cf21f552618b7
gpiechnik2 <grzegorz.piechnik99@gmail.com>
2023-07-18 12:37:40 +0100

    OneclassSVM refactored

--- module.go
+++ module.go
@@ -31,26 +31,24 @@ func (*Anomaly) Lof(data []DataPoint, threshold float64) []LOFResult {
 	return anomalies
 }
 
-func (*Anomaly) OneClassSvm(trainData []DataPoint, data []DataPoint) []DataPoint {
+func (*Anomaly) OneClassSvm(trainData []DataPoint, testData []DataPoint) []DataPoint {
 	var anomalies []DataPoint
 	
 	convertedTrainData := make([][]float64, len(trainData))
-	for i, point := range trainData {
-		convertedTrainData[i] = []float64{point.X, point.Y}
+	for i, dp := range trainData {
+		convertedTrainData[i] = []float64{dp.X, dp.Y}
 	}
 
-	ocsvm := NewOneClassSVM(rbfKernel, 0.01)
-	ocsvm.Fit(convertedTrainData, 0.9)
+	// Tworzenie i dopasowywanie modelu One-Class SVM
+	ocsvm := NewOneClassSVM(rbfKernel)
+	ocsvm.Fit(convertedTrainData)
 
-	convertedTestData := make([][]float64, len(data))
-	for i, point := range data {
-		convertedTestData[i] = []float64{point.X, point.Y}
-	}
-
-	for _, instance := range data {
-		convertedInstance := []float64{instance.X, instance.Y}
+	// Wykrywanie anomalii w danych testowych
+	for _, dp := range testData {
+		convertedInstance := []float64{dp.X, dp.Y}
 		predicted := ocsvm.Predict(convertedInstance)
-		if predicted != -1 {
+
+		if predicted == -1 {
 			anomalies = append(anomalies, instance)
 		}
 	}
--- oneClassSvm.go
+++ oneClassSvm.go
@@ -2,72 +2,52 @@ package anomaly
 
 import (
 	"math"
-	"sort"
 )
 
 type OneClassSVM struct {
 	SupportVectors [][]float64
 	Bias           float64
 	Kernel         func(x, y []float64) float64
-	Gamma          float64
 }
 
-func NewOneClassSVM(kernel func(x, y []float64) float64, gamma float64) *OneClassSVM {
+func NewOneClassSVM(kernel func(x, y []float64) float64) *OneClassSVM {
 	return &OneClassSVM{
 		SupportVectors: nil,
 		Bias:           0,
 		Kernel:         kernel,
-		Gamma:          gamma,
 	}
 }
 
-func (svm *OneClassSVM) Fit(X [][]float64, nu float64) {
+func (svm *OneClassSVM) Fit(X [][]float64) {
 	numSamples := len(X)
 
-	// Initialization of the gramian variable
+	// initialization of the gramian matrix
 	gramMatrix := make([][]float64, numSamples)
 	for i := range gramMatrix {
 		gramMatrix[i] = make([]float64, numSamples)
 	}
 
-	// Calculation of the gramma
+	// calculation of the gramian value
 	for i := 0; i < numSamples; i++ {
 		for j := 0; j < numSamples; j++ {
 			gramMatrix[i][j] = svm.Kernel(X[i], X[j])
 		}
 	}
 
-	// Calculating the bias factor
+	// calculating the bias value
 	sumAlphas := 0.0
 	for i := 0; i < numSamples; i++ {
 		sumAlphas += gramMatrix[i][i]
 	}
 	svm.Bias = sumAlphas / float64(numSamples)
 
-	// Writing the support vectors
+	// storing support vectors
 	svm.SupportVectors = make([][]float64, 0)
 	for i := 0; i < numSamples; i++ {
 		if gramMatrix[i][i] >= svm.Bias {
 			svm.SupportVectors = append(svm.SupportVectors, X[i])
 		}
 	}
-
-	numSupportVectors := len(svm.SupportVectors)
-	numSupportVectorsInBoundary := int(math.Round(nu * float64(numSupportVectors)))
-	if numSupportVectorsInBoundary == 0 {
-		numSupportVectorsInBoundary = 1
-	}
-
-	// Sort the support vectors by distance from the decision boundary.
-	distancesFromBoundary := make([]float64, numSupportVectors)
-	for i, sv := range svm.SupportVectors {
-		dist := svm.Kernel(sv, sv) - 2*svm.Bias
-		distancesFromBoundary[i] = dist
-	}
-	sort.Float64s(distancesFromBoundary)
-
-	// Set the decision boundary to correspond to the distance from the boundary
-	svm.Bias = distancesFromBoundary[numSupportVectorsInBoundary-1]
 }
 
 func (svm *OneClassSVM) Predict(X []float64) int {
@@ -90,5 +70,6 @@ func rbfKernel(x, y []float64) float64 {
 		diff := x[i] - y[i]
 		squaredEuclideanDistance += diff * diff
 	}
-	return math.Exp(-squaredEuclideanDistance)
+	gamma := 1.0 / float64(len(x))
+	return math.Exp(-gamma * squaredEuclideanDistance)
 }
